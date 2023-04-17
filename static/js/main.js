ff4962348ebcb7f05ef4303f60bc93ef16abe0db
abou <abd.koumare@gmail.com>
2023-04-17 00:11:27 +0000

    initialized the project

---
+++ static/js/main.js
@@ -0,0 +1,163 @@
+var hamburgerBtn = document.querySelector("#hamburger-btn");
+var mobileNavigation = document.querySelector("#mobile-nav");
+
+hamburgerBtn.addEventListener("click", function () {
+    // add .is_activated which cause display of navigation container using css sibling selector '+'
+    this.classList.toggle("is_activated");
+
+    // remove .has_shadow when hamburger is activated
+    mobileNavigation.classList.toggle("has_shadow");
+})
+
+
+var mobileNavigationContext = document.querySelector("#mobile-menu-container");
+
+document.addEventListener('click', function (event) {
+
+    // Check if the target of the click is inside the navigation context
+    const isClickInsideNav = mobileNavigationContext.contains(event.target) || hamburgerBtn.contains(event.target);
+
+    // If the target of the click is outside the navigation context, close the hamburger menu
+    if (!isClickInsideNav) {
+        hamburgerBtn.classList.remove('is_activated');
+        mobileNavigation.classList.toggle("has_shadow");
+    }
+});
+
+
+// Closing the menu when the menu anchor link is clicked
+var deactivateMenuAtClickBtn = document.querySelectorAll('.close-mobile-menu-trg');
+
+deactivateMenuAtClickBtn.forEach(function (el) {
+    el.addEventListener('click', function () {
+        hamburgerBtn.classList.remove('is_activated');
+    })
+})
+
+
+/**
+
+ const breadcrumbContainer = document.createElement('div');
+ breadcrumbContainer.style.position = 'fixed';
+ breadcrumbContainer.style.bottom = '10px';
+ breadcrumbContainer.style.right = '10px';
+ breadcrumbContainer.style.backgroundColor = '#fff';
+ breadcrumbContainer.style.padding = '10px';
+ document.body.appendChild(breadcrumbContainer);
+ var sections = document.querySelectorAll('.section-content');
+
+ // Loop through each section
+ sections.forEach((section) => {
+    // Get the section's ID
+    const sectionId = section.getAttribute('id');
+
+    // Create a new breadcrumb element
+    const breadcrumb = document.createElement('span');
+    breadcrumb.innerHTML = ` > ${sectionId}`;
+
+    // Insert the breadcrumb into the breadcrumb container
+    breadcrumbContainer.appendChild(breadcrumb);
+});
+ // Add an event listener for scrolling
+ window.addEventListener('scroll', () => {
+
+    // Loop through each section
+    sections.forEach((section) => {
+        // Get the section's ID
+        const sectionId = section.getAttribute('id');
+
+        // Check if the section is currently in view
+        if (section.getBoundingClientRect().top <= window.innerHeight / 2) {
+            // Loop through each breadcrumb and update its style
+            const breadcrumbs = breadcrumbContainer.querySelectorAll('span');
+            breadcrumbs.forEach((breadcrumb) => {
+                breadcrumb.style.fontWeight = 'normal';
+            });
+
+            // Find the breadcrumb for the current section and update its style
+            const breadcrumbsArray = Array.from(breadcrumbs);
+            const currentBreadcrumb = breadcrumbsArray.find((breadcrumb) => breadcrumb.textContent.includes(sectionId));
+            currentBreadcrumb.style.fontWeight = 'bold';
+        }
+    });
+}) */
+
+
+const GA_MEASUREMENT_ID =  'G-5TJ6JV3KNZ';
+const GA_MEASUREMENT_URL = 'https://www.googletagmanager.com/gtag/js?id=' + GA_MEASUREMENT_ID;
+// Check if the user has previously accepted cookies
+const cookieAccepted = localStorage.getItem('cookieAccepted');
+
+if (cookieAccepted) {
+    // User has already accepted cookies, enable Google Analytics
+    enableGoogleAnalytics();
+} else {
+    // User has not yet accepted cookies, show the cookie agreement modal
+    const cookieModal = document.getElementById('cookie-modal');
+    cookieModal.style.display = 'block';
+
+    const acceptButton = document.getElementById('accept-cookies');
+    acceptButton.addEventListener('click', () => {
+        // User accepts cookies, enable Google Analytics and save acceptance to localStorage
+        enableGoogleAnalytics();
+        localStorage.setItem('cookieAccepted', true);
+
+        // Close the modal
+        cookieModal.style.display = 'none';
+    });
+
+    const declineButton = document.getElementById('decline-cookies');
+    declineButton.addEventListener('click', () => {
+        // User declines cookies, disable Google Analytics and do not save acceptance to localStorage
+        disableGoogleAnalytics();
+        localStorage.setItem('cookieAccepted', false);
+        // Close the modal
+        cookieModal.style.display = 'none';
+    });
+}
+
+function enableGoogleAnalytics() {
+    // Show the Google Analytics scripts
+    const googleAnalyticsScripts = document.querySelectorAll('.google-analytics');
+    googleAnalyticsScripts.forEach((script) => {
+        script.style.display = 'block';
+    });
+
+    // Insert the Google Analytics tracking code into the document
+    const gaScript = document.createElement('script');
+    gaScript.async = true;
+    gaScript.src = GA_MEASUREMENT_URL
+    document.head.appendChild(gaScript);
+
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+    gtag('config', GA_MEASUREMENT_ID);
+}
+
+function disableGoogleAnalytics() {
+    // Hide the Google Analytics scripts
+    const googleAnalyticsScripts = document.querySelectorAll('.google-analytics');
+    googleAnalyticsScripts.forEach((script) => {
+        script.style.display = 'none';
+    });
+
+    // Remove the Google Analytics tracking code from the document
+
+    const gaScript = document.querySelector('script[src=GA_MEASUREMENT_URL]');
+    if (gaScript) {
+        gaScript.remove();
+    }
+
+    // Remove any existing Google Analytics cookies
+    document.cookie.split(";").forEach(function (cookie) {
+        const cookieName = cookie.split("=")[0];
+        if (cookieName.indexOf("_ga") === 0 || cookieName.indexOf("_gid") === 0) {
+            document.cookie = cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        }
+    });
+}
